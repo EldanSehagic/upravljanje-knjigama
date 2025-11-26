46a5936a1c813e43146f8ded10aeb0e6edbdc544
Eldan <eldan.sehagic.23@size.ba>
2025-11-26 12:30:16 +0100

    Dodane dvije ObicneKnjige i dvije RijetkeKnjige u  Main

--- src/main/java/ba/biblioteka/Main.java
+++ src/main/java/ba/biblioteka/Main.java
@@ -6,5 +6,10 @@ import java.util.Map;
 public class Main {
     static void main(String[] args) {
         Map<String,Knjiga> biblioteka = new HashMap<>();
+
+        ObicnaKnjiga o1 = new ObicnaKnjiga("E111",60);
+        ObicnaKnjiga o2 = new ObicnaKnjiga("E112",50);
+        RijetkaKnjiga r1 = new RijetkaKnjiga("R111",90);
+        RijetkaKnjiga  r2 = new RijetkaKnjiga("R112",120);
     }
 }
