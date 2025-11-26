a4227b97f4937643018f85706561e0d3c47283aa
Eldan <eldan.sehagic.23@size.ba>
2025-11-26 11:59:56 +0100

    Dodani atributi String ISBN i double brojStranica,dodana aptraktna metoda double izracunajVrijednost() u Knjiga.java klasu.Dodan konstruktor i getteri

--- src/main/java/ba/biblioteka/Knjiga.java
+++ src/main/java/ba/biblioteka/Knjiga.java
@@ -1,4 +1,21 @@
 package ba.biblioteka;
 
 public abstract class Knjiga {
+    protected String ISBN;
+    protected double brojStranica;
+
+    public abstract double izracunajVrijednost();
+
+    public Knjiga(String ISBN, double brojStranica) {
+        this.ISBN = ISBN;
+        this.brojStranica = brojStranica;
+    }
+
+    public String getISBN() {
+        return ISBN;
+    }
+
+    public double getBrojStranica() {
+        return brojStranica;
+    }
 }
