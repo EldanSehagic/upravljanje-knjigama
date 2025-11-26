2909c29fcc228588252b4a21ff546f205b92fbc8
Eldan <eldan.sehagic.23@size.ba>
2025-11-26 12:20:20 +0100

    Dodana funkcionalnost racunanja vrijednosti u metodi izracunajVrijednost() u klasi ObicnaKnjiga

--- src/main/java/ba/biblioteka/ObicnaKnjiga.java
+++ src/main/java/ba/biblioteka/ObicnaKnjiga.java
@@ -3,7 +3,7 @@ package ba.biblioteka;
 public class ObicnaKnjiga extends Knjiga implements Status {
     @Override
     public double izracunajVrijednost() {
-        return 0;
+        return brojStranica*0.5;
     }
 
     public ObicnaKnjiga(String ISBN, double brojStranica) {
