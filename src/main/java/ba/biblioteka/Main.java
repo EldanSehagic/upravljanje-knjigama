859941a375a10b58eb2102140a610f5c38468282
Eldan <eldan.sehagic.23@size.ba>
2025-11-26 12:28:38 +0100

    Dodana HashMapa u  Main

--- src/main/java/ba/biblioteka/Main.java
+++ src/main/java/ba/biblioteka/Main.java
@@ -1,7 +1,10 @@
 package ba.biblioteka;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
     static void main(String[] args) {
-        
+        Map<String,Knjiga> biblioteka = new HashMap<>();
     }
 }
